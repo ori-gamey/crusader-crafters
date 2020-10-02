862fcf2b16bec5eb40f59d79b558ec04318dc854
ethsmith <ethan.smith1845@gmail.com>
2020-10-02 18:35:06 -0500

    first commit

---
+++ src/main/java/gg/clergyofbarry/crusadercrafters/CrusaderCrafters.java
@@ -0,0 +1,17 @@
+package gg.clergyofbarry.crusadercrafters;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CrusaderCrafters extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
